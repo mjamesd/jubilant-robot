ac1b3a939116ffe91b4907bb19d51cd4f4a61836
Mark Drummond <mjamesd@gmail.com>
2022-01-16 20:40:59 -0800

    Added controllers for regular routes and API calls  - [x] Correct home route data is returned.  - [ ] TO DO: write other controller routes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,16 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./home');
 const userRoutes = require('./user');
 const postRoutes = require('./post');
 const commentRoutes = require('./comment');
 
-
-router.use('/', homeRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
-router.use('/api', apiRoutes);
 
 module.exports = router;
